aa6b49cc892ba2c3458fe4270ea809796a9ff3bf
SON NGUYEN <myantj3m@gmail.com>
2018-09-19 10:26:57 +0700

    add aglorithm bubble sort

---
+++ src/main/java/bubble/sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package bubble.sort;
+
+public class BubbleSort {
+    static int[] list = {2, 3, 22, 5, 6, 1, -2, 13, 34, 12};
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+    }
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int i = 1; i < list.length && needNextPass; i++) {
+            needNextPass=false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+}
+
